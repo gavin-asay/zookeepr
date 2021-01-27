212a41e6174ba901267fe15a929a94fffb4a77be
Gavin Asay <gavin.asay@gmail.com>
2021-01-27 02:06:14 -0700

    API post

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+//parse incoming JSON data
+app.use(express.json());
 const { animals } = require('./data/animals');
 
 function filterByQuery(query, animalsArray) {
@@ -43,6 +50,26 @@ function findById(id, animalsArray) {
 	return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+	const animal = body;
+	animalsArray.push(animal);
+	fs.writeFileSync(
+		path.join(__dirname, './data/animals.json'),
+		JSON.stringify({ animals: animalsArray }, null, 2)
+	);
+
+	return animal;
+}
+
+function validateAnimal(animal) {
+	if (!animal.name || typeof animal.name !== 'string') return false;
+	if (!animal.species || typeof animal.species !== 'string') return false;
+	if (!animal.diet || typeof animal.diet !== 'string') return false;
+	if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits))
+		return false;
+	return true;
+}
+
 app.get('/api/animals', (req, res) => {
 	let results = animals;
 	if (req.query) {
@@ -55,6 +82,18 @@ app.get('/api/animals/:id', (req, res) => {
 	const result = findById(req.params.id, animals);
 	if (result) res.json(result);
 	else res.sendStatus(404);
+});
+
+app.post('/api/animals', (req, res) => {
+	req.body.id = animals.length.toString();
+
+	if (!validateAnimal(req.body))
+		res.status(400).send('The animal is not properly formatted.');
+	else {
+		const animal = createNewAnimal(req.body, animals);
+
+		res.json(animals);
+	}
 });
 
 app.listen(PORT, () => {
